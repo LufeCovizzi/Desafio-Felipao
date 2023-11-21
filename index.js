17dfa101319d6404fda71bd2680cbf98c815f7cb
LufeCovizzi <149106018+LufeCovizzi@users.noreply.github.com>
2023-11-21 19:59:49 -0300

    Segundo commit
    Update index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+nomeHeroi = prompt ("Digite o nome do seu herói: ")
+levelHeroi = prompt ("Qual a quantidade de XP do seu herói? ")
+
+if (levelHeroi <= 1000) {
+    console.log ("O Herói de nome" + nomeHeroi + "está no nível Ferro")
+} 
+if (levelHeroi > 1000 && levelHeroi <=2000){
+    console.log ("O Herói de nome" + nomeHeroi + "está no nível Bronze")
+} 
+if (levelHeroi > 2000 && levelHeroi <=5000){
+    console.log ("O Herói de nome" + nomeHeroi + "está no nível Prata")
+} 
+if (levelHeroi > 5000 && levelHeroi <=7000){
+    console.log ("O Herói de nome" + nomeHeroi + "está no nível Ouro")
+} 
+if (levelHeroi > 7000 && levelHeroi <=8000){
+    console.log ("O Herói de nome" + nomeHeroi + "está no nível Platina")
+} 
+if (levelHeroi >8000 && levelHeroi <=9000){
+    console.log ("O Herói de nome" + nomeHeroi + "está no nível Ascendente")
+} 
+if (levelHeroi >9000 && levelHeroi <=10000){
+    console.log ("O Herói de nome" + nomeHeroi + "está no nível Imortal")
+} 
+if (levelHeroi > 10000) {
+    console.log ("O Herói de nome" + nomeHeroi + "está no nível Radiante")
+}
