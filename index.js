40c9734c6964c6ce39192c5691cf3b464c02250f
LufeCovizzi <149106018+LufeCovizzi@users.noreply.github.com>
2023-11-21 20:07:04 -0300

    Terceiro commit
    Melhorando concatenação

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ nomeHeroi = prompt ("Digite o nome do seu herói: ")
 levelHeroi = prompt ("Qual a quantidade de XP do seu herói? ")
 
 if (levelHeroi <= 1000) {
-    console.log ("O Herói de nome" + nomeHeroi + "está no nível Ferro")
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível Ferro")
 } 
 if (levelHeroi > 1000 && levelHeroi <=2000){
-    console.log ("O Herói de nome" + nomeHeroi + "está no nível Bronze")
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível Bronze")
 } 
 if (levelHeroi > 2000 && levelHeroi <=5000){
-    console.log ("O Herói de nome" + nomeHeroi + "está no nível Prata")
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível Prata")
 } 
 if (levelHeroi > 5000 && levelHeroi <=7000){
-    console.log ("O Herói de nome" + nomeHeroi + "está no nível Ouro")
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível Ouro")
 } 
 if (levelHeroi > 7000 && levelHeroi <=8000){
-    console.log ("O Herói de nome" + nomeHeroi + "está no nível Platina")
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível Platina")
 } 
 if (levelHeroi >8000 && levelHeroi <=9000){
-    console.log ("O Herói de nome" + nomeHeroi + "está no nível Ascendente")
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível Ascendente")
 } 
 if (levelHeroi >9000 && levelHeroi <=10000){
-    console.log ("O Herói de nome" + nomeHeroi + "está no nível Imortal")
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível Imortal")
 } 
 if (levelHeroi > 10000) {
-    console.log ("O Herói de nome" + nomeHeroi + "está no nível Radiante")
+    console.log ("O Herói de nome " + nomeHeroi + "está no nível Radiante")
 }
